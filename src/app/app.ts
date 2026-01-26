36c159f913e2ecd4071d22f2732e8588522d8a5a
lim-mark20 <mblim1220@gmail.com>
2026-01-26 16:51:34 +0800

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import { User } from './user.model';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Httpclient } from './httpclient';
-
+import { Product } from './product.model';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -12,12 +12,16 @@ import { Httpclient } from './httpclient';
 export class App {
   protected readonly title = signal('ng-httpclient-demo');
   httpusers: User[] = [];
+  httpproducts: Product[] = [];
 
   constructor(private httpClient: Httpclient) {}
 
   ngOnInit() {
     this.httpClient.getUsersRemotely().subscribe((data) => {
       this.httpusers = data;
+    });
+    this.httpClient.getProductsRemotely().subscribe((data) => {
+      this.httpproducts = data;
     });
   }
 }
--- src/app/httpclient.ts
+++ src/app/httpclient.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { User } from './user.model';
-
+import { Product } from './product.model';
 @Injectable({
   providedIn: 'root',
 })
@@ -12,5 +13,12 @@ export class Httpclient {
 
   getUsersRemotely(): Observable<User[]> {
     return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users');
+  }
+
+  getProductsRemotely(): Observable<Product[]> {
+    return this.http.get<{ products: Product[] }>('https://dummyjson.com/products')
+      .pipe(
+        map(response => response.products)
+      );
   }
 }
