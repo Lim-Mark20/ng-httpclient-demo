ea4226ef40e270569527179b67c689e9dcee00ad
lim-mark20 <mblim1220@gmail.com>
2026-02-02 15:30:03 +0800

    update: to cache

--- src/app/httpclient.ts
+++ src/app/httpclient.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { User } from './user.model';
 import { Product } from './product.model';
@@ -9,16 +9,28 @@ import { Product } from './product.model';
 })
 
 export class Httpclient {
+  private usersUrl = 'https://jsonplaceholder.typicode.com/users';
+  private productsUrl = 'https://dummyjson.com/products';
   constructor(private http: HttpClient) {}
 
   getUsersRemotely(): Observable<User[]> {
-    return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users');
+    const cachedUsers = localStorage.getItem('users');
+    if (cachedUsers) {
+      return of(JSON.parse(cachedUsers));
+    }
+    return this.http.get<User[]>(this.usersUrl).pipe(
+      tap(users => localStorage.setItem('users', JSON.stringify(users)))
+    );
   }
 
   getProductsRemotely(): Observable<Product[]> {
-    return this.http.get<{ products: Product[] }>('https://dummyjson.com/products')
-      .pipe(
-        map(response => response.products)
-      );
+    const cachedProducts = localStorage.getItem('products');
+    if (cachedProducts) {
+      return of(JSON.parse(cachedProducts));
+    }
+    return this.http.get<{ products: Product[] }>(this.productsUrl).pipe(
+      map(response => response.products),
+      tap(products => localStorage.setItem('products', JSON.stringify(products)))
+    );
   }
 }
